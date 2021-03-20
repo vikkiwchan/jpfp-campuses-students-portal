505335c30bd065143f5c9aabc9c33129b50fcb87
Vikki Chan <vikkiwchan@gmail.com>
2021-03-20 14:53:53 -0500

    data layer set up

---
+++ seed.js
@@ -0,0 +1,96 @@
+const campuses = [
+  {
+    name: 'University Park Campus',
+    address: 'Los Angeles, CA 90007',
+  },
+  {
+    name: 'Health Sciences Campus',
+    address: ' Zonal Avenue, Biggy St, Los Angeles, CA 90033',
+  },
+  {
+    name: 'USC Orange County Center',
+    address: '2300 Michelson Dr, Irvine, CA 92612',
+  },
+  {
+    name: 'USC Wrigley Marine Science Center',
+    address: '1 Big Fisherman Cove, Avalon, CA 90704',
+  },
+];
+
+const students = [
+  {
+    firstName: 'Amanda',
+    lastName: 'Boehm',
+    email: 'amanda.boehm@usc.edu',
+    gpa: 3.8,
+    campusId: 1,
+  },
+  {
+    firstName: 'Christian',
+    lastName: 'Sporer',
+    email: 'christian.sporer@usc.edu',
+    gpa: 3.3,
+    campusId: 1,
+  },
+  {
+    firstName: 'Jovany',
+    lastName: 'Carroll',
+    email: 'jovany.carroll@usc.edu',
+    gpa: 4.0,
+    campusId: 2,
+  },
+  {
+    firstName: 'Art',
+    lastName: 'Pouros',
+    email: 'art.pouros@usc.edu',
+    gpa: 2.8,
+    campusId: 3,
+  },
+  {
+    firstName: 'Paula',
+    lastName: 'Satterfield',
+    email: 'paula.satterfield@usc.edu',
+    gpa: 3.6,
+    campusId: 3,
+  },
+  {
+    firstName: 'Francesca',
+    lastName: 'Waelchi',
+    email: 'francesca.waelchi@usc.edu',
+    gpa: 3.9,
+    campusId: 4,
+  },
+  {
+    firstName: 'Bryon',
+    lastName: 'Bernier',
+    email: 'bryon.bernier@usc.edu',
+    gpa: 2.5,
+    campusId: 3,
+  },
+  {
+    firstName: 'Fabian',
+    lastName: 'Mosciski',
+    email: 'fabian.mosciski@usc.edu',
+    gpa: 3.4,
+    campusId: 2,
+  },
+  {
+    firstName: 'Willms',
+    lastName: 'Bart',
+    email: 'willms.bart@usc.edu',
+    gpa: 3.8,
+    campusId: 3,
+  },
+  {
+    firstName: 'Abner',
+    lastName: 'Kertzman',
+    email: 'abner.kertzman@usc.edu',
+    gpa: 3.2,
+    campusId: 4,
+  },
+];
+
+module.exports = {
+  campuses,
+  students,
+};
